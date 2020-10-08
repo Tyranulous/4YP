899ff397ac2fe54cee9e034dcec6e238d818954e
Joschua Spiedel-Johnson <joschua@spiedel.co.uk>
2020-10-08 14:32:29 +0100

    telescope nested for loop bash script works

---
+++ Scripts/telescopeparams.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for cf in 300 800 1400; do
+# central freq - mhz
+    for bw in 50 200 400; do
+# bandwidth - mhz
+        for tsamp in 64 256 1024; do
+# time sampling - microseconds
+            for chans in 2024 4096; do
+# number of chans
+                echo "cf:$cf  bw:'$bw  tsamp:$tsamp  chans:$chans"
+            done
+        done
+    done
+done
+
+
+
