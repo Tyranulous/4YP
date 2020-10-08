db07e76ff611e54c59a9652d27e820a441548547
Tyranulous <tyranulous@gmail.com>
2020-10-08 17:27:14 +0100

    planned out tasks for telescope params script

--- Scripts/telescopeparams.sh
+++ Scripts/telescopeparams.sh
@@ -10,6 +10,12 @@ for cf in 300 800 1400; do
 # number of chans
                 echo "cf:$cf  bw:'$bw  tsamp:$tsamp  chans:$chans"
                 # code to run astro accelerate with these params goes here
+                # 1. generate ddplan probably by executing ddplan.py with params -n -b -t -f as the corresponding values
+		# 2.import telescope params into c file somehow
+		# 3. import ddplan into astro accelerate somehow
+		# 4. profit
+		# 5. run jan's script to check possible combinations of hpc params
+		# 6. save data somewhere usefull with unique names for each run
             done
         done
     done
